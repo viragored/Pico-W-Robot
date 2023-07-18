eb8a26c6cfb1844668f1f308962d0155e6ec8767
viragored <itsdaveandanne@gmail.com>
2023-07-18 20:19:29 +1200

    Add files via upload

--- 4WD_DRV8833.py
+++ 4WD_DRV8833.py
@@ -4,10 +4,8 @@ from time import sleep
 import machine, neopixel
 from machine import Pin, PWM
 from secret import ssid,password,hostname
-led = Pin("LED", Pin.OUT) # onboard LED
-np = neopixel.NeoPixel(Pin(28), 1) # Set Pin 28,1 neopixel
-kip = 0.25
-bright = 7
+led = Pin(28, Pin.OUT)
+kip = 0.025
 
 frq = 20 # lower number gets more torque at the wheels
 dutymax = 65535 # highest duty cycle value
@@ -18,22 +16,30 @@ currentstep = 1 # initialise at slow speed
 duty = int(dutymin+(dutyrange/10)*currentstep)
 lastaction = "" # the module to run after speed change
 
-pin1 = Pin(18)
-in1 = machine.PWM(pin1)
+in1 = machine.PWM(Pin(18)) # fwd
 in1.freq(frq)
-pin2 = Pin(19)
-in2 = machine.PWM(pin2)
+in2 = machine.PWM(Pin(19)) # bwd
 in2.freq(frq)
-pin3 = Pin(20)
-in3 = machine.PWM(pin3)
+in3 = machine.PWM(Pin(20)) # fwd
 in3.freq(frq)
-pin4 = Pin(21)
-in4 = machine.PWM(pin4)
+in4 = machine.PWM(Pin(21)) # bwd
 in4.freq(frq)
 
+def LEDon(): 
+    led.on()
+def LEDoff():
+    led.off()
+def blink (n):
+    for times in range (n):
+        led.on()    
+        sleep(kip)
+        led.off()
+        sleep(kip)
+    led.on()    
 def fwd():
     global lastaction
     lastaction = "f"
+    blink (5)
     in1.duty_u16(duty)
     in3.duty_u16(duty)
     in2.duty_u16(0)
@@ -41,6 +47,7 @@ def fwd():
 def bwd():
     global lastaction
     lastaction = "b"
+    blink (5)
     in1.duty_u16(0)
     in3.duty_u16(0)
     in2.duty_u16(duty)
@@ -48,30 +55,33 @@ def bwd():
 def stop():
     global lastaction
     lastaction = "s"
+    LEDoff()
     in1.duty_u16(0)
     in3.duty_u16(0)
     in2.duty_u16(0)
     in4.duty_u16(0)
 def right():
     global lastaction
-    lastaction = "l"
-    in1.duty_u16(duty)
+    lastaction = "r"
+    blink (5)
+    in1.duty_u16(int(dutymax*0.65))
     in3.duty_u16(0)
     in2.duty_u16(0)
-    in4.duty_u16(duty)
+    in4.duty_u16(int(dutymax*0.65))
 def left():
     global lastaction
-    lastaction = "r"
+    lastaction = "l"
+    blink (5)
     in1.duty_u16(0)
-    in3.duty_u16(duty)
-    in2.duty_u16(duty)
+    in3.duty_u16(int(dutymax*0.65))
+    in2.duty_u16(int(dutymax*0.65))
     in4.duty_u16(0)
 def slow():
     global currentstep, duty
     if (currentstep > 0):
         currentstep -= 1
         duty = int(dutymin+(dutyrange/10)*currentstep)
-#         duty = int(dutymax*(speedsteps[currentstep]))
+    blink (5)
     print ("Slower, currentstep now:",currentstep,"duty now",duty)
     if lastaction == "f":
         fwd()
@@ -88,6 +98,7 @@ def fast():
     if (currentstep < steps):
         currentstep += 1
         duty = int(dutymin+(dutyrange/10)*currentstep)
+    blink (5)
     print ("Faster, currentstep now:",currentstep,"duty now",duty)
     if lastaction == "f":
         fwd()
@@ -99,15 +110,6 @@ def fast():
         left()
     elif lastaction == "s":
         stop()
-def LEDon(rv,gv,bv): # turns on LED pixel, red grn blue brightness values 
-    np[0] = (rv,gv,bv)        
-    np.write()
-def LEDoff(): # turns off all pixels
-    np[0] = (0,0,0)        
-    np.write()
-#Stop the robot as soon as possible
-stop()
-    
 def connect():
     #Connect to WLAN
     wlan = network.WLAN(network.STA_IF)
@@ -118,16 +120,10 @@ def connect():
         print('Waiting for connection...')
         sleep(1)
     ip = wlan.ifconfig()[0]
-    for x in range (9): # indicate network connected
-        LEDon (bright,bright,bright)
-        sleep (0.25)
-        LEDoff()
-        sleep (0.25)
     print(f'Connected on {ip}')
+    blink (10)
     return ip
-    
-def open_socket(ip):
-    # Open a socket
+def open_socket(ip): # Open a socket
     address = (ip, 80)
     connection = socket.socket()
     connection.bind(address)
@@ -232,15 +228,17 @@ def serve(connection):
         html = webpage()
         client.send(html)
         client.close()
-LEDon (0,bright,0) # red = power
+#Stop the robot as soon as possible
+stop()
+    
 try:
     ip = connect()
     connection = open_socket(ip)
     serve(connection)
     sleep(0.25)
 except KeyboardInterrupt:
-    LEDoff()
     stop()
+    LEDoff()
     machine.reset()
 
     
